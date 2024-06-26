b4ae59eca103115c17c0c7cdbbdd75883e42b4ff
M1stery <lautarocepero3@gmail.com>
2024-06-26 01:05:35 -0300

    added database

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,28 +1,3 @@
 from flask import Blueprint, jsonify, request
 
 tasks_blueprints = Blueprint("tasks", __name__)
-
-# Get all tasks
-@tasks_blueprints.route("/tasks", methods=["GET"])
-def get_tasks():
-    ""
-    
-# Get task by ID
-@tasks_blueprints.route("/tasks/<int:task_id>", methods=["GET"])
-def get_task(task_id):
-    ""
-    
-# Create a task
-@tasks_blueprints.route("/tasks", methods=["POST"])
-def create_task():
-    ""
-    
-# Delete a task
-@tasks_blueprints.route("/tasks/<int:task_id>", methods=["DELETE"])
-def delete_task(task_id):
-    ""
-    
-# Update a task
-@tasks_blueprints.route("/tasks/<int:task_id>", methods=["PUT"])
-def update_task(task_id):
-    ""
