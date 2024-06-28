af74e344353fdb2d33a5d82fd7b168d60807e26b
M1stery <lautarocepero3@gmail.com>
2024-06-28 04:05:05 -0300

    fixed typo

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from models import db
 from dotenv import load_dotenv
+from task_blueprint import task_blueprint
+from user_blueprint import user_blueprint
 import os
 
 if __name__ == "__main__":
@@ -8,8 +10,8 @@ if __name__ == "__main__":
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.register_blueprint(user_bp)
-    app.register_blueprint(task_bp)
+    app.register_blueprint(task_blueprint)
+    app.register_blueprint(user_blueprint)
     db.init_app(app)
     with app.app_context():
         db.create_all()
